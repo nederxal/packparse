8966df83acc024fba6fdd05915cec819593ffe83
nederxal <adelanchy@protonmail.ch>
2018-05-08 23:09:24 +0200

    premier test

---
+++ parse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+1 lister les packs
+2 pour chaque pack lister les DWI --> garder :
+    le nom de la musique / vitesse / stepper / difficultés
+    sazchant que 1 diff = 1 stepper la plupart du temps 
+
+    (banner a récup en bonux)
