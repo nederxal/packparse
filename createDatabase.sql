5f4df99407bab3b4b17363b456f00666db21a377
nederxal <adelanchy@protonmail.ch>
2018-06-08 17:54:52 +0200

    Ré-écriture complète
    - Refonte de la base en 4 tables (pour des features futures)
    - refonte de la partie insertion db
    - ajout d'une partie pour NE PAS réinserer un pack déjà existants (et les songs avec)
    - prise en compte des fichiers UTF-8 / des lignes vides
    - amélioration pour la récupération du titre de la musique
    
    Axes d'amélioration :
    - meilleure prise en charge ouverture smfile (fait 2 fois actuellement)
    - créer le site pour exploiter les données
    - trouver comment extraire le plus efficacement possible le BPM
    - ajouter les banners pour le style ?

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,13 +1,43 @@
-CREATE TABLE dataSongs (
-    id           INTEGER       PRIMARY KEY AUTOINCREMENT
-                               UNIQUE
-                               NOT NULL,
-    pack         VARCHAR (255) DEFAULT PackLess,
-    songTitle    VARCHAR (255) NOT NULL,
-    songSpeed    INTEGER,
-    songStepper  VARCHAR (255) DEFAULT BLANK_ARTISTE,
-    singlePlay   BOOLEAN       NOT NULL
-                               DEFAULT (1),
-    songDiffName VARCHAR (255) NOT NULL,
-    songDiff     INTEGER       NOT NULL
+CREATE TABLE pack (
+    id         INTEGER       PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
+                             UNIQUE,
+    pack_name  VARCHAR (255) NOT NULL
+                             UNIQUE,
+    entry_date DATE
 );
+
+CREATE TABLE stepper (
+    id           INTEGER       PRIMARY KEY AUTOINCREMENT
+                               UNIQUE,
+    stepper_name VARCHAR (255) UNIQUE
+                               NOT NULL
+);
+
+CREATE TABLE difficulty (
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    difficulty_name         UNIQUE
+);
+
+INSERT INTO difficulty (difficulty_name)
+VALUES ("Beginner"), ("Easy"), ("Medium"), ("Hard"), ("Challenge"), ("Edit")
+
+
+CREATE TABLE songs (
+    id                 INTEGER       PRIMARY KEY AUTOINCREMENT
+                                     UNIQUE,
+    fk_pack_name                     REFERENCES pack (id) ON DELETE CASCADE
+                                                          ON UPDATE CASCADE
+                                     NOT NULL,
+    song_name          VARCHAR (255) NOT NULL,
+    speed              INTEGER,
+    fk_stepper_name                  REFERENCES stepper (id) ON DELETE NO ACTION
+                                                             ON UPDATE CASCADE
+                                     NOT NULL,
+    single             BOOLEAN       NOT NULL
+                                     DEFAULT True,
+    fk_difficulty_name               NOT NULL
+                                     REFERENCES difficulty (id) ON DELETE SET NULL
+                                                                ON UPDATE CASCADE,
+    difficulty_block   INTEGER       NOT NULL
+);
+
--- parse.py
+++ parse.py
@@ -1,83 +1,121 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 # pour chaque pack lister les fichier SM --> garder :
-#	le pack parent 			--> le répertoire parsé
-#	le nom de la musique	--> ligne #TITLE
-#	vitesse 				--> ligne #DISPLAYBPM ou BPMS ou autre chose ... à voir ... on laisse de côté pour le moment.
-#	stepper 				-->
-#	difficultés	+ nb de blocks	-->
-#	sachant que 1 diff = 1 stepper la plupart du temps 
-#	(banner a récup en bonux --> https://stackoverflow.com/questions/3715493/encoding-an-image-file-with-base64?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa)
+#    le pack parent             --> le répertoire parsé
+#    le nom de la musique    --> ligne #TITLE ou #TITLETRANSLIT (plus complet si existant)
+#    vitesse                 --> ligne #DISPLAYBPM ou BPMS ou autre chose ... à voir ... on laisse de côté pour le moment.
+#    stepper                 -->
+#    difficultés    + nb de blocks    -->
+#    sachant que 1 diff = 1 stepper la plupart du temps 
+#    (banner a récup en bonux --> https://stackoverflow.com/questions/3715493/encoding-an-image-file-with-base64?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa)
 
 
 import os
 import sys
 import re
 import sqlite3
+from datetime import datetime
 
 def main():
-	os.chdir("pack")
-	packList = os.listdir(".")
-	songsInfo = []
-	for pack in packList:
-		packName = pack
-		for dirPath, dirName, simFiles in os.walk(packName):
-			for simFile in simFiles:
-				if simFile.endswith(".sm"):
-					songInfos = sim_extract(packName, os.path.join(dirPath,simFile))
-					insert_song(songInfos)
-	sys.exit(0)
+    os.chdir("pack")
+    pack_list = os.listdir(".")
+    for pack_name in pack_list:
+        # On ajoute le pack dans la table correspondante et on
+        # récupère son ID pour la table songs
+        fk_pack_name = db_insert("pack", pack_name)
+        for dirPath, dirName, sim_files in os.walk(pack_name):
+            for sim_file in sim_files:
+                if sim_file.endswith(".sm") and fk_pack_name > 0:
+                    title = sim_extract(os.path.join(dirPath,sim_file))
+                    diff_extract(os.path.join(dirPath,sim_file),
+                                fk_pack_name,
+                                title)
+    return 0
 
-###début bloc extract données SMFILE
-def sim_extract(_packName, _simFile):
-	lines = []
-	with open (_simFile, 'r') as simFile:
-		for line in simFile:
-			if line.startswith("#TITLE:"):
-				line = re.sub("[\[\]#;]", '', line).strip()
-				line_tmp = re.split(":", line)
-				line = ' - '.join(line_tmp[1:])
-				lines.append(_packName)
-				lines.append(line)
-				lines.append('null') #servira un jour pour la speed
-				songInfos = diff_extract(lines, _simFile)
-	simFile.close()
-	return songInfos
-
-
-def diff_extract(_lines, _simFile):
-	tmpLines = []
-	songInfos = []
-	with open (_simFile, 'r') as simFile:
-		for line in simFile:
-			if re.match(r'[ \t]', line) and not re.search("//", line):
-				line = re.sub("[\[\]#;:\t]", '', line).strip()
-				tmpLines.append(line)
-	simFile.close()
-	del tmpLines[4::5]
-	while tmpLines:
-		lines = []
-		lines = list(_lines)
-		if re.match('dance-single', tmpLines[0]):
-			lines.append(1)
-		else:
-			lines.append(0)
-		tmpLines.pop(0)
-		lines.append(tmpLines.pop(0))
-		lines.append(tmpLines.pop(0))
-		lines.append(tmpLines.pop(0))
-		songInfos.append(tuple(lines))
-	return songInfos
+# Extraction du titre de la chanson
+# possibilité d'étendre avec BPMs / bpm change, banner etc 
+def sim_extract(sim_file):
+    with open (sim_file, 'r', encoding='utf-8-sig') as sim_file_tmp:
+        for line in sim_file_tmp:
+            if line.startswith("#TITLE:"):
+                title = re.sub("#TITLE:", '', line, count=1).strip()
+            if line.startswith("#TITLETRANSLIT:"):
+                title_tpm = re.sub("#TITLETRANSLIT:", '', line, count=1).strip()
+                if len(title_tpm) > 1:
+                    title = title_tpm
+    sim_file_tmp.close()
+    title = re.sub(";", '', title)
+    return title
 
 
-def insert_song(songsInfo):
-	conn = sqlite3.connect("../songs.db")
-	c = conn.cursor()
-	c.executemany('INSERT INTO dataSongs(pack,songTitle,songSpeed,singlePlay,songStepper,songDiffName,songDiff) VALUES(?,?,?,?,?,?,?)',songsInfo)
-	conn.commit()
-	c.close()
+# Extraction difficulté / stepper artiste / double-single / difficulté blocks
+def diff_extract(sim_file, pack, title):
+    lines_tmp = []
+    init_tab = [pack, title, "SPEED"]
+    with open (sim_file, 'r', encoding='utf-8-sig') as sim_file_tmp:
+        for line in sim_file_tmp:
+            if re.match('[ \t]', line) \
+                        and re.search(":", line) \
+                        and not re.search("//", line):
+                line = re.sub("[\[\]#;:\t]", '', line).strip()
+                lines_tmp.append(line)
+    sim_file_tmp.close()
+    del lines_tmp[4::5]
+    while lines_tmp:
+        song_info_tab = []
+        song_info_tab = list(init_tab)
+        if re.match('dance-single', lines_tmp[0]):
+            song_info_tab.append("True")
+        else:
+            song_info_tab.append("False")
+        lines_tmp.pop(0)
+        fk_stepper_name = db_insert("stepper", lines_tmp[0])
+        lines_tmp.pop(0)
+        song_info_tab.append(fk_stepper_name)
+        fk_difficulty_name = db_insert("difficulty", lines_tmp[0])
+        lines_tmp.pop(0)
+        song_info_tab.append(fk_difficulty_name)
+        song_info_tab.append(lines_tmp.pop(0))
+        db_insert("songs", *song_info_tab)
 
+def db_insert(table, *args):
+    conn = sqlite3.connect("../songs.db")
+    c = conn.cursor()
+    if table == 'pack':
+        try:
+            c.execute("INSERT INTO pack(pack_name, entry_date) VALUES (?,?)", 
+                    (args[0], datetime.now().year))
+            conn.commit()
+            c.execute('SELECT id FROM pack WHERE pack_name = ?', (args))
+        except sqlite3.IntegrityError as e:
+            #on fait zapper l'intégration de toute les musiques de ce pack
+            print(args[0]," probablment déjà présent dans la base :", e) 
+    elif table == 'stepper':
+        try:
+            c.execute('INSERT INTO stepper(stepper_name) VALUES (?)', (args))
+        except sqlite3.IntegrityError as e:
+            pass #print("Erreur sur stepper ",e)
+        conn.commit()
+        c.execute('SELECT id FROM stepper WHERE stepper_name = ?', (args))
+    elif table == 'difficulty':
+        c.execute('SELECT id FROM difficulty WHERE difficulty_name = ?', (args))
+    elif table == 'songs':
+        c.execute("INSERT INTO songs(fk_pack_name, \
+                                    song_name, \
+                                    speed, \
+                                    single, \
+                                    fk_stepper_name,\
+                                    fk_difficulty_name,\
+                                    difficulty_block)  \
+                                    VALUES (?,?,?,?,?,?,?)", (args))
+        conn.commit()
+    value_fk = c.fetchone()
+    if not value_fk:
+        value_fk = [0]
+    c.close()
+    return value_fk[0]
 
 if __name__ == '__main__':
     main()
     sys.exit(0)
+    
