ec9ac1714027fe0a738c43ce9d41a107388b97e1
adelanchy <adelanchy@oxya.com>
2019-11-21 10:42:08 +0100

    [Update] Création de la première partie web, recherche de pack possible et ça fonctionne bien utilisation de milligram comme framweork CSS

--- weblistor/weblistor/forms.py
+++ weblistor/weblistor/forms.py
@@ -2,9 +2,28 @@
 # -*- coding:utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import TextField, IntegerField, BooleanField
-from wtforms.validators import InputRequired
+from wtforms import TextField, IntegerField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Optional
 
 
 class SearchPack(FlaskForm):
-    pack_name = TextField("Looking for a pack ?", [InputRequired()])
+    pack_name = TextField("Pack name :", [InputRequired()])
+
+
+class SearchSong(FlaskForm):
+    song_name = TextField("Song name :", [Optional()])
+    stepper = TextField("Step artist :", [Optional()])
+    speed_low = IntegerField("Min BPM :", [Optional()])
+    speed_high = IntegerField("Max BPM :", [Optional()])
+    double = BooleanField("Double / Couple ?")
+    block = IntegerField("Block :", [Optional()])
+    diff = SelectField('Difficulté ?',
+                       [Optional()],
+                       choices=[('%', 'Any diff'),
+                                ('1', 'Beginner'),
+                                ('2', 'Easy'),
+                                ('3', 'Medium'),
+                                ('4', 'Hard'),
+                                ('5', 'Challenge'),
+                                ('6', 'Edit')],
+                       default='%')
